f9758eed624725d81ac11a89fe41d879c36810c4
andrewkolos <andrewrkolos@gmail.com>
2019-06-09 02:31:03 -0500

    add keyboard manager module

---
+++ src/keyboard.ts
@@ -0,0 +1,19 @@
+export class KeyboardManager {
+
+  private keyDownStates: { [keyCode: number]: boolean };
+
+  public constructor() {
+    this.keyDownStates = {};
+
+    document.body.addEventListener('keydown', (e: KeyboardEvent) => {
+      this.keyDownStates[e.keyCode] = true;
+    });
+    document.body.addEventListener('keyup', (e: KeyboardEvent) => {
+      this.keyDownStates[e.keyCode] = false;
+    });
+  }
+
+  public isKeyDown(keycode: number): boolean {
+    return this.keyDownStates[keycode];
+  }
+}
