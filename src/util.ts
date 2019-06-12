23f48cc8696f21b877ec5c0b3050789d7c165503
andrewkolos <andrewrkolos@gmail.com>
2019-06-11 19:21:09 -0500

    add image data for thre renderer

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import Three from 'three';
+
 export type DeepReadonly<T> =
   T extends (infer R)[] ? DeepReadonlyArray<R> :
   T extends Function ? T :
@@ -9,3 +11,15 @@ export interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> { }
 export type DeepReadonlyObject<T> = {
   readonly [P in keyof T]: DeepReadonly<T[P]>;
 };
+
+export function makeTextureFromBase64Image(data: string) {
+  const image = new Image();
+  image.src = data;
+  const texture = new Three.Texture();
+  texture.image = image;
+  image.onload= function() {
+    texture.needsUpdate = true;
+  }
+
+  return texture;
+}
